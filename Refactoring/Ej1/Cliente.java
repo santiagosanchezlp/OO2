fd2cd5316f7cf9a321510bd86010496089a2d243
SANTIAGO GONZALO SANCHEZ <sgsanchez.90@gmail.com>
2025-03-29 20:47:24 -0300

    Ej1 corregido

---
+++ Refactoring/Ej1/Cliente.java
@@ -0,0 +1,20 @@
+public class Cliente {
+/** 
+* Retorna el límite de crédito del cliente
+*/
+//    public double lmtCrdt() {...
+    public double limiteCredito(){}
+/** 
+* Retorna el monto facturado al cliente desde la fecha f1 a la fecha f2
+*/
+//    protected double mtFcE(LocalDate f1, LocalDate f2) {...
+    protected double montoFacturado(DateLapse periodo){}
+/** 
+* Retorna el monto cobrado al cliente desde la fecha f1 a la fecha f2
+*/
+//    private double mtCbE(LocalDate f1, LocalDate f2) {...
+    private double montoCobrado(DateLapse periodo){}
+
+// mtFcE y mtCbE: DATA CLUMPS? Depende de cuánto más se usa Intervalo
+// TODOS: RENAME METHOD
+}
